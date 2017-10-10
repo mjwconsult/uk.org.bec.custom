d708e1e55b4dcdd3b963fea82f9356fc0e7bbf59
Matthew Wire <devel@mrwire.co.uk>
2017-10-10 23:29:32 +0100

    Enable BCA membership selection

--- js/contribution8.js
+++ js/contribution8.js
@@ -3,7 +3,7 @@ CRM.$(function($) {
   var becMembership = CRM.$('#price_3');
   var bcaMembership = CRM.$('#price_4');
   becMembership.select2().enable(false);
-  bcaMembership.select2().enable(false);
+  bcaMembership.select2().enable(true);
   becMembership.select2({width: '75%'});
   bcaMembership.select2({width: '75%'});
 });
